38517a5ef181eb57690fea6cb3c05c1ec62611a8
Vorfall <alekseevegorc@gmail.com>
2021-03-02 10:15:40 +0300

    final solution

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 module.exports = function reverse (n) {
-  
-str=n+""
-arr = str.split('')
-arr.reverse()
+        
+            str=n+""
+            arr = str.split('')
+            arr.reverse()
+          
+            newStr = arr.join("")
+            newStr = parseInt(newStr)
       
 
-          return arr
+          return newStr
 }
